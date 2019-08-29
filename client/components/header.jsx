c70b93aa84b029c44b9e4a238b986094471588eb
James Yamagata <wilsonhj@uci.edu>
2019-08-28 18:19:53 -0700

    finished view cart summary.

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function Header(props) {
   return (
-    <header className="text-center header border border-dark container-fluid">
+    <header className="text-center header border border-dark bg-primary text-white container-fluid">
       <div className="row justify-content-between">
         <h1 className="d-flex p-2">Wicked Sales</h1>
-        <i className="fas fa-shopping-cart d-infline-flex p-4"> {props.cartItemCount} items </i>
+        <i className="fas fa-shopping-cart d-infline-flex p-4" onClick={props.viewClick}> {props.cartItemCount} items </i>
       </div>
     </header>
   );
--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function ProductListItem(props) {
   return (
-    <div className="card" key={props.id} onClick={() => props.setView('details', { id: props.id })} >
-      <img className = "img-fluid" src={props.image}></img>
+    <div className="card product-list-item" key={props.id} onClick={() => props.setView('details', { id: props.id })} >
+      <img className = "img-fluid frontpage-image" src={props.image}></img>
       <div className="card-body">
         <h5 className="card-title">{props.name}</h5>
         <h6 className="card-subtitle mb-2 text-muted">${props.price}</h6>
