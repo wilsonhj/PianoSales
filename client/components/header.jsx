138c26f0baa0b79f89358a253bab69517b649cd4
James Yamagata <wilsonhj@uci.edu>
2019-09-24 10:40:58 -0700

    pulled dev to BE

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
 
-<<<<<<< HEAD
-export default function Header() {
-  return (
-    <header className="text-center">Wicked Sales</header>
-=======
 export default function Header(props) {
   return (
     <header className="text-center header border border-dark bg-primary text-white container-fluid">
@@ -13,6 +8,5 @@ export default function Header(props) {
         <i className="fas fa-shopping-cart d-infline-flex p-4" onClick={props.viewClick}> {props.cartItemCount} items </i>
       </div>
     </header>
->>>>>>> e3e4f034ae4149eb17ee9095f5ff70ebc926b90f
   );
 }
